dca7801c73b7d1a9f38452cc229121f4a66de08c
SoyOscarRH <soyoscarrh@gmail.com>
2020-10-04 21:39:02 -0500

    Updates

---
+++ precache-manifest.3edb5f765d11a73adacf04ea537911ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2abc4754d7c0c679dd3b8ee120beced",
+    "url": "/Tetris_v1/index.html"
+  },
+  {
+    "revision": "c2db157d483da39ec074",
+    "url": "/Tetris_v1/static/css/main.dad78432.chunk.css"
+  },
+  {
+    "revision": "e144ed58225fa5fed45e",
+    "url": "/Tetris_v1/static/js/2.fd2fa731.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tetris_v1/static/js/2.fd2fa731.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2db157d483da39ec074",
+    "url": "/Tetris_v1/static/js/main.3fecacfd.chunk.js"
+  },
+  {
+    "revision": "cc8ef27314127da1e788",
+    "url": "/Tetris_v1/static/js/runtime-main.155d1a07.js"
+  }
+]);
