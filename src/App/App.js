b22c16722664aaabb669ca112a45998793277c12
leilanimr <40678759+leilanimr@users.noreply.github.com>
2020-10-20 00:27:58 -0500

    Comentarios y puntuacion final

--- src/App/App.js
+++ src/App/App.js
@@ -26,48 +26,50 @@ const App = () => {
   }, [score])
 
   useEffect(() => {
-    pieces.current.forEach(piece => piece.moveX(pieces.current))
-    if (!movingPiece.current.canMoveX(pieces.current)) {
-      setSpeed(originalSpeed - level * 50)
-      if (!movingPiece.current.getPixels().some(([x, y]) => x === 0)) {
+    pieces.current.forEach(piece => piece.moveX(pieces.current))//todas se caen
+    if (!movingPiece.current.canMoveX(pieces.current)) { //ocupa todas las piezas para generar el tablero y ver si se puede mover
+      setSpeed(originalSpeed - level * 50) //reset velocidad por si s
+      if (!movingPiece.current.getPixels().some(([x, y]) => x === 0)) { //Verifica si esta la pieza hasta arriba para ver si el juego continua
         const board = getEmptyBoard()
         pieces.current.forEach(piece => piece.getPixels().forEach(([x, y]) => (board[x][y] = piece.type)))
 
         for (let height = 0; height < board.length; ++height) {
-          const is_full_line = board[height].every(pixel => pixel !== "empty")
+          const is_full_line = board[height].every(pixel => pixel !== "empty") //verifica si la linea esta llena
           if (is_full_line) {
-            setScore(score => score + 10)
-            pieces.current.forEach(piece => piece.deletePixelHeight(height))
+            setScore(score => score + 10) //aumenta puntuacion
+            pieces.current.forEach(piece => piece.deletePixelHeight(height)) //elimina pixeles de esa linea
           }
-          pieces.current = pieces.current.filter(piece => piece.getPixels().length !== 0)
+          pieces.current = pieces.current.filter(piece => piece.getPixels().length !== 0) //solo piezas con pixeles
         }
 
-        movingPiece.current = getRandomPiece()
-        pieces.current.push(movingPiece.current)
+        movingPiece.current = getRandomPiece()//genera ptra pieza
+        pieces.current.push(movingPiece.current) //anade la moving pis al conjunto de las demas piezas
       } else {
-        setGame(false)
+        setGame(false) //acaba juego
       }
     }
-  }, [time])
+  }, [time]) //cada que el time cambie
 
   useEffect(() => {
     document.addEventListener("keypress", ({ key }) => {
-      if (key === "a") movingPiece.current.moveLeft(pieces.current)
+      if (key === "a") movingPiece.current.moveLeft(pieces.current) //recibe todas las piezas para ver si se puede mover
       if (key === "d") movingPiece.current.moveRight(pieces.current)
       if (key === "w") movingPiece.current.rotate()
-      if (key === "s") setSpeed(25)
+      if (key === "s") setSpeed(25) //baje mas rapido
 
       if (key === "a" || key === "d" || key === "w") repaint()
     })
   }, [repaint])
 
-  const board = getEmptyBoard()
-  pieces.current.forEach(piece => piece.getPixels().forEach(([x, y]) => (board[x][y] = piece.type)))
+  const board = getEmptyBoard() 
+  pieces.current.forEach(piece => piece.getPixels().forEach(([x, y]) => (board[x][y] = piece.type)))//pinta las piezas en el board que se acaba de generar
 
   if (!isGameRunning) {
     return (
       <>
         <h1>Game Over</h1>
+        <h1>{score}</h1>
+
       </>
     )
   }
